4515e504ca6a3b60af5c8326d4dd92d71fceb470
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-14 08:29:55 +0300

    added routing to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UsersComponent } from './users/users.component';
-import { ReposComponent } from './repos/repos.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    ReposComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../sevices/search.service'
 
 @Component({
   selector: 'app-users',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchService:SearchService) {
+    // this.searchService.getUsers();
+  }
 
   ngOnInit(): void {
   }
