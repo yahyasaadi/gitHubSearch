21208eb9eddda172b0d9b58df1449f47d71ff1fa
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-16 11:16:57 +0300

    added a section to the app html to display my info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,18 @@ export class AppComponent {
     this.getMyGit();
   }
 
+  public username:any;
+  public followers:any;
+  public following:any;
+  public repositories:any;
+
   getMyGit(){
     return this.searchService.getMyData().subscribe(data=>{
       console.log(data);
+      this.username = data.login;
+      this.followers = data.followers;
+      this.following = data.following;
+      this.repositories = data.public_repos;
     })
   }
 }
